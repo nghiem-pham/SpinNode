f63cc1442d8766bcd308f88e1b33aabd14cc7dcf
LittleTom388 <nghiempham388@gmail.com>
2026-04-06 23:45:50 -0500

    boostrap spring boot backend app

---
+++ server/src/main/java/com/npham/spinnode/SpinNodeApplication.java
@@ -0,0 +1,13 @@
+package com.npham.spinnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpinNodeApplication.class, args);
+	}
+
+}
